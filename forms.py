f9327d2ed855c78a62654c7b8115e031a6372cdb
Samathingamajig <samuelgunter3@gmail.com>
2020-02-04 01:01:45 -0600

    Changed around some form stuff

--- forms.py
+++ forms.py
@@ -1,5 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, StringField, SubmitField, validators, ValidationError
+from wtforms import TextAreaField, StringField, SubmitField, validators, ValidationError, SelectField
+
+
+def reason_check(form, field):
+    if field.data == "error":
+        raise ValidationError('Please select a reason.')
 
 
 class ContactForm(FlaskForm):
@@ -9,6 +14,12 @@ class ContactForm(FlaskForm):
         "Please enter your email address."), validators.Email("Please enter your email address.")])
     subject = StringField(
         "Subject",  [validators.Required("Please enter a subject.")])
+    reason = SelectField(u'Reason for Contacting', [validators.Required("Please select a reason."), reason_check], choices=[
+        ('error', 'Please select a reason for contacting'),
+        ('Issue', 'Issue'),
+        ('Suggestion', 'Suggestion'),
+        ('Press', 'Press Inquiry'),
+        ('Other', 'Other, please specify in text box below')])
     message = TextAreaField(
         "Message",  [validators.Required("Please enter a message.")])
     submit = SubmitField("Send")
